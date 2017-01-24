107fb594fd39377e02e42819620edc06a55699a7
Joep Kokkeler <j.kokkeler@gmail.com>
2017-01-24 11:47:20 +0100

    Working app with style

---
+++ src/main/java/nl/nljug/jcp/jsr375/core/MyApp.java
@@ -0,0 +1,9 @@
+package nl.nljug.jcp.jsr375.core;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/rest")
+public class MyApp extends Application {
+    
+}
---
+++ src/main/java/nl/nljug/jcp/jsr375/web/MyCtrl.java
@@ -0,0 +1,16 @@
+package nl.nljug.jcp.jsr375.web;
+ 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+ 
+@Path("/")
+public class MyCtrl {
+ 
+    @GET
+    @Path("/")
+    public Response test() {
+        return Response.status(200).entity("test page ok").build();
+    }
+ 
+}
